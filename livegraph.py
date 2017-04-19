35df4e28fe9dec9b8e780d1225e16790bcd5bb2d
Ke Yue <keeyue@gmail.com>
2017-04-19 23:13:29 +0000

    Update livegraph.py

--- livegraph.py
+++ livegraph.py
@@ -1,213 +1,206 @@
+#!/usr/bin/env python
 
-import sys
-import os
-from PyQt4 import QtGui
-from PyQt4 import QtCore
-import functools
-import numpy as np
-import random as rd
-import matplotlib
-matplotlib.use("Qt4Agg")
-from matplotlib.figure import Figure
-from matplotlib.animation import TimedAnimation
-from matplotlib.lines import Line2D
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-import time
-import threading
+#############################################################################
+##
+## Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
+##
+## This file is part of the example classes of the Qt Toolkit.
+##
+## This file may be used under the terms of the GNU General Public
+## License version 2.0 as published by the Free Software Foundation
+## and appearing in the file LICENSE.GPL included in the packaging of
+## this file.  Please review the following information to ensure GNU
+## General Public Licensing requirements will be met:
+## http://www.trolltech.com/products/qt/opensource.html
+##
+## If you are unsure which license is appropriate for your use, please
+## review the following information:
+## http://www.trolltech.com/products/qt/licensing.html or contact the
+## sales department at sales@trolltech.com.
+##
+## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+##
+#############################################################################
+
+from PyQt4 import QtCore, QtGui
 
 
-
-def setCustomSize(x, width, height):
-    sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-    sizePolicy.setHorizontalStretch(0)
-    sizePolicy.setVerticalStretch(0)
-    sizePolicy.setHeightForWidth(x.sizePolicy().hasHeightForWidth())
-    x.setSizePolicy(sizePolicy)
-    x.setMinimumSize(QtCore.QSize(width, height))
-    x.setMaximumSize(QtCore.QSize(width, height))
-
-''''''
-
-class CustomMainWindow(QtGui.QMainWindow):
-
+class ImageViewer(QtGui.QMainWindow):
     def __init__(self):
+        super(ImageViewer, self).__init__()
 
-        super(CustomMainWindow, self).__init__()
+        self.printer = QtGui.QPrinter()
+        self.scaleFactor = 0.0
 
-        # Define the geometry of the main window
-        self.setGeometry(300, 300, 800, 400)
-        self.setWindowTitle("my first window")
+        self.imageLabel = QtGui.QLabel()
+        self.imageLabel.setBackgroundRole(QtGui.QPalette.Base)
+        self.imageLabel.setSizePolicy(QtGui.QSizePolicy.Ignored,
+                QtGui.QSizePolicy.Ignored)
+        self.imageLabel.setScaledContents(True)
 
-        # Create FRAME_A
-        self.FRAME_A = QtGui.QFrame(self)
-        self.FRAME_A.setStyleSheet("QWidget { background-color: %s }" % QtGui.QColor(210,210,235,255).name())
-        self.LAYOUT_A = QtGui.QGridLayout()
-        self.FRAME_A.setLayout(self.LAYOUT_A)
-        self.setCentralWidget(self.FRAME_A)
+        self.scrollArea = QtGui.QScrollArea()
+        self.scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
+        self.scrollArea.setWidget(self.imageLabel)
+        self.setCentralWidget(self.scrollArea)
 
-        # Place the zoom button
-        self.zoomBtn = QtGui.QPushButton(text = 'zoom')
-        setCustomSize(self.zoomBtn, 100, 50)
-        self.zoomBtn.clicked.connect(self.zoomBtnAction)
-        self.LAYOUT_A.addWidget(self.zoomBtn, *(0,0))
+        self.createActions()
+        self.createMenus()
 
-        # Place the matplotlib figure
-        self.myFig = CustomFigCanvas()
-        self.LAYOUT_A.addWidget(self.myFig, *(0,1))
+        self.setWindowTitle("Image Viewer")
+        self.resize(500, 400)
 
-        # Add the callbackfunc to ..
-        myDataLoop = threading.Thread(name = 'myDataLoop', target = dataSendLoop, args = (self.addData_callbackFunc,))
-        myDataLoop.daemon = True
-        myDataLoop.start()
+    def open(self):
+        # fileName = QtGui.QFileDialog.getOpenFileName(self, "Open File",
+        #         QtCore.QDir.currentPath())
+        # if fileName:
+        #     image = QtGui.QImage(fileName)
+        #     if image.isNull():
+        #         QtGui.QMessageBox.information(self, "Image Viewer",
+        #                 "Cannot load %s." % fileName)
+        #         return
 
-        self.show()
-
-    ''''''
-
-
-    def zoomBtnAction(self):
-        print("zoom in")
-        self.myFig.zoomIn(5)
-
-    ''''''
-
-    def addData_callbackFunc(self, value):
-        # print("Add data: " + str(value))
-        self.myFig.addData(value)
+        from scipy import misc
+        from PIL.ImageQt import ImageQt
+        import scipy
+        
+        l = misc.face()
+        # p = QtGui.QLabel()
+        q = QtGui.QPixmap.fromImage(ImageQt(scipy.misc.toimage(l)))
 
 
+        self.imageLabel.setPixmap(q)
 
-''' End Class '''
+        # self.imageLabel.setPixmap(QtGui.QPixmap.fromImage(image))
+        self.scaleFactor = 1.0
+
+        self.printAct.setEnabled(True)
+        self.fitToWindowAct.setEnabled(True)
+        self.updateActions()
+
+        if not self.fitToWindowAct.isChecked():
+            self.imageLabel.adjustSize()
+
+    def print_(self):
+        dialog = QtGui.QPrintDialog(self.printer, self)
+        if dialog.exec_():
+            painter = QtGui.QPainter(self.printer)
+            rect = painter.viewport()
+            size = self.imageLabel.pixmap().size()
+            size.scale(rect.size(), QtCore.Qt.KeepAspectRatio)
+            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
+            painter.setWindow(self.imageLabel.pixmap().rect())
+            painter.drawPixmap(0, 0, self.imageLabel.pixmap())
+
+    def zoomIn(self):
+        self.scaleImage(1.25)
+
+    def zoomOut(self):
+        self.scaleImage(0.8)
+
+    def normalSize(self):
+        self.imageLabel.adjustSize()
+        self.scaleFactor = 1.0
+
+    def fitToWindow(self):
+        fitToWindow = self.fitToWindowAct.isChecked()
+        self.scrollArea.setWidgetResizable(fitToWindow)
+        if not fitToWindow:
+            self.normalSize()
+
+        self.updateActions()
+
+    def about(self):
+        QtGui.QMessageBox.about(self, "About Image Viewer",
+                "<p>The <b>Image Viewer</b> example shows how to combine "
+                "QLabel and QScrollArea to display an image. QLabel is "
+                "typically used for displaying text, but it can also display "
+                "an image. QScrollArea provides a scrolling view around "
+                "another widget. If the child widget exceeds the size of the "
+                "frame, QScrollArea automatically provides scroll bars.</p>"
+                "<p>The example demonstrates how QLabel's ability to scale "
+                "its contents (QLabel.scaledContents), and QScrollArea's "
+                "ability to automatically resize its contents "
+                "(QScrollArea.widgetResizable), can be used to implement "
+                "zooming and scaling features.</p>"
+                "<p>In addition the example shows how to use QPainter to "
+                "print an image.</p>")
+
+    def createActions(self):
+        self.openAct = QtGui.QAction("&Open...", self, shortcut="Ctrl+O",
+                triggered=self.open)
+
+        self.printAct = QtGui.QAction("&Print...", self, shortcut="Ctrl+P",
+                enabled=False, triggered=self.print_)
+
+        self.exitAct = QtGui.QAction("E&xit", self, shortcut="Ctrl+Q",
+                triggered=self.close)
+
+        self.zoomInAct = QtGui.QAction("Zoom &In (25%)", self,
+                shortcut="Ctrl++", enabled=False, triggered=self.zoomIn)
+
+        self.zoomOutAct = QtGui.QAction("Zoom &Out (25%)", self,
+                shortcut="Ctrl+-", enabled=False, triggered=self.zoomOut)
+
+        self.normalSizeAct = QtGui.QAction("&Normal Size", self,
+                shortcut="Ctrl+S", enabled=False, triggered=self.normalSize)
+
+        self.fitToWindowAct = QtGui.QAction("&Fit to Window", self,
+                enabled=False, checkable=True, shortcut="Ctrl+F",
+                triggered=self.fitToWindow)
+
+        self.aboutAct = QtGui.QAction("&About", self, triggered=self.about)
+
+        self.aboutQtAct = QtGui.QAction("About &Qt", self,
+                triggered=QtGui.qApp.aboutQt)
+
+    def createMenus(self):
+        self.fileMenu = QtGui.QMenu("&File", self)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.printAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.viewMenu = QtGui.QMenu("&View", self)
+        self.viewMenu.addAction(self.zoomInAct)
+        self.viewMenu.addAction(self.zoomOutAct)
+        self.viewMenu.addAction(self.normalSizeAct)
+        self.viewMenu.addSeparator()
+        self.viewMenu.addAction(self.fitToWindowAct)
+
+        self.helpMenu = QtGui.QMenu("&Help", self)
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+        self.menuBar().addMenu(self.fileMenu)
+        self.menuBar().addMenu(self.viewMenu)
+        self.menuBar().addMenu(self.helpMenu)
+
+    def updateActions(self):
+        self.zoomInAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.zoomOutAct.setEnabled(not self.fitToWindowAct.isChecked())
+        self.normalSizeAct.setEnabled(not self.fitToWindowAct.isChecked())
+
+    def scaleImage(self, factor):
+        self.scaleFactor *= factor
+        self.imageLabel.resize(self.scaleFactor * self.imageLabel.pixmap().size())
+
+        self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), factor)
+        self.adjustScrollBar(self.scrollArea.verticalScrollBar(), factor)
+
+        self.zoomInAct.setEnabled(self.scaleFactor < 3.0)
+        self.zoomOutAct.setEnabled(self.scaleFactor > 0.333)
+
+    def adjustScrollBar(self, scrollBar, factor):
+        scrollBar.setValue(int(factor * scrollBar.value()
+                                + ((factor - 1) * scrollBar.pageStep()/2)))
 
 
-class CustomFigCanvas(FigureCanvas, TimedAnimation):
+if __name__ == '__main__':
 
-    def __init__(self):
+    import sys
 
-        self.addedData = []
-        print(matplotlib.__version__)
-
-        # The data
-        self.xlim = 200
-        self.n = np.linspace(0, self.xlim - 1, self.xlim)
-        a = []
-        b = []
-        a.append(2.0)
-        a.append(4.0)
-        a.append(2.0)
-        b.append(4.0)
-        b.append(3.0)
-        b.append(4.0)
-        self.y = (self.n * 0.0) + 50
-
-        # The window
-        self.fig = Figure(figsize=(5,5), dpi=100)
-        self.ax1 = self.fig.add_subplot(111)
-
-
-        # self.ax1 settings
-        self.ax1.set_xlabel('time')
-        self.ax1.set_ylabel('raw data')
-        self.line1 = Line2D([], [], color='blue')
-        self.line1_tail = Line2D([], [], color='red', linewidth=2)
-        self.line1_head = Line2D([], [], color='red', marker='o', markeredgecolor='r')
-        self.ax1.add_line(self.line1)
-        self.ax1.add_line(self.line1_tail)
-        self.ax1.add_line(self.line1_head)
-        self.ax1.set_xlim(0, self.xlim - 1)
-        self.ax1.set_ylim(0, 100)
-
-
-        FigureCanvas.__init__(self, self.fig)
-        TimedAnimation.__init__(self, self.fig, interval = 50, blit = True)
-
-    def new_frame_seq(self):
-        return iter(range(self.n.size))
-
-    def _init_draw(self):
-        lines = [self.line1, self.line1_tail, self.line1_head]
-        for l in lines:
-            l.set_data([], [])
-
-    def addData(self, value):
-        self.addedData.append(value)
-
-    def zoomIn(self, value):
-        bottom = self.ax1.get_ylim()[0]
-        top = self.ax1.get_ylim()[1]
-        bottom += value
-        top -= value
-        self.ax1.set_ylim(bottom,top)
-        self.draw()
-
-
-    def _step(self, *args):
-        # Extends the _step() method for the TimedAnimation class.
-        try:
-            TimedAnimation._step(self, *args)
-        except Exception as e:
-            self.abc += 1
-            print(str(self.abc))
-            TimedAnimation._stop(self)
-            pass
-
-    def _draw_frame(self, framedata):
-        margin = 2
-        while(len(self.addedData) > 0):
-            self.y = np.roll(self.y, -1)
-            self.y[-1] = self.addedData[0]
-            del(self.addedData[0])
-
-
-        self.line1.set_data(self.n[ 0 : self.n.size - margin ], self.y[ 0 : self.n.size - margin ])
-        self.line1_tail.set_data(np.append(self.n[-10:-1 - margin], self.n[-1 - margin]), np.append(self.y[-10:-1 - margin], self.y[-1 - margin]))
-        self.line1_head.set_data(self.n[-1 - margin], self.y[-1 - margin])
-        self._drawn_artists = [self.line1, self.line1_tail, self.line1_head]
-
-
-
-''' End Class '''
-
-
-# You need to setup a signal slot mechanism, to
-# send data to your GUI in a thread-safe way.
-# Believe me, if you don't do this right, things
-# go very very wrong..
-class Communicate(QtCore.QObject):
-    data_signal = QtCore.pyqtSignal(float)
-
-''' End Class '''
-
-
-
-def dataSendLoop(addData_callbackFunc):
-    # Setup the signal-slot mechanism.
-    mySrc = Communicate()
-    mySrc.data_signal.connect(addData_callbackFunc)
-
-    # Simulate some data
-    n = np.linspace(0, 499, 500)
-    y = 50 + 25*(np.sin(n / 8.3)) + 10*(np.sin(n / 7.5)) - 5*(np.sin(n / 1.5))
-    i = 0
-
-    while(True):
-        if(i > 499):
-            i = 0
-        time.sleep(0.1)
-        mySrc.data_signal.emit(y[i]) # <- Here you emit a signal!
-        i += 1
-    ###
-###
-
-
-
-
-if __name__== '__main__':
     app = QtGui.QApplication(sys.argv)
-    QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Plastique'))
-    myGUI = CustomMainWindow()
-
-
+    imageViewer = ImageViewer()
+    imageViewer.show()
     sys.exit(app.exec_())
-
-''''''
